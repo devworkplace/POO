391793614b9fd39139e3541644dcd7f2981a1f7f
SERGIO ANTONIO A BARBOSA <saabarbosa@gmail.com>
2021-07-02 00:18:32 -0300

    atualizar

---
+++ Aula02_ExemploDeAtributos-Metodos/ContaCorrente.java
@@ -0,0 +1,48 @@
+public class ContaCorrente {
+
+  // Atributos
+  int numeroConta;
+  String nomeBanco;
+  String nomeCliente;
+  float saldo;
+
+  //Métodos
+
+  public void cadastrarConta(int nConta, String nBanco, String nCliente) 
+  {
+    numeroConta = nConta;
+    nomeBanco = nBanco;
+    nomeCliente = nCliente;
+  }
+  
+  void depositar(float valor){
+    if (valor > 0)
+    {
+      saldo += valor;
+    }
+  }
+  
+  boolean sacar(float valor){
+    if (saldo >= valor){
+      saldo -= valor;
+      return true;
+    }
+      return false;
+  }
+  
+  void atualizarNomeCliente(String nCliente){
+    nomeCliente = nCliente;
+  }
+  
+  void atualizarBanco(String nBanco){
+    nomeBanco = nBanco;
+  }
+  
+  void atualizarNumeroConta(int numConta){
+    numeroConta = numConta;
+  }
+ 
+  float obterSaldo(){
+    return saldo;
+  }
+}
--- Aula02_ExemploDeAtributos-Metodos/Teste.java
+++ Aula02_ExemploDeAtributos-Metodos/Teste.java
@@ -32,7 +32,28 @@ public class Teste {
     aluno.faltas = 15;
     String status = aluno.analisarSituacao(aluno.nota1, aluno.nota2, aluno.faltas);
     System.out.println("Nome: " + aluno.nome + "\n" + "Status = " + status);
+    System.out.println("-----------------------------");
 
+    // Instanciando a classe bola e atribuindo valores
+    Bola bola = new Bola();
+    bola.tamanho = 10;
+    bola.cor = "branco";
+    bola.esvaziar();
+    System.out.println("Cor da Bola: " + bola.cor + "\n" + "Cheia: " + bola.cheia);
+
+    System.out.println("-----------------------------");
+    System.out.println("Oi, criando uma conta corrente...");
+ 
+    ContaCorrente c1 = new ContaCorrente();
+    c1.cadastrarConta(500178,"bb","Anderson Silva");
+    System.out.println("Saldo: " + c1.obterSaldo());
+    
+    // movimentando a conta corrente
+    c1.depositar(3000);
+    System.out.println("Saldo: " + c1.obterSaldo());
+
+    c1.sacar(80);
+    System.out.println("Saldo: " + c1.obterSaldo());
   }
   
 }
